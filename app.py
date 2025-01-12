29df0718c317c1b5f3e9f5a68b6a68c79439c643
Nikhil-2x <nikhilyadav101513@gmail.com>
2025-01-13 00:53:12 +0530

    update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 connection = pymysql.connect(
     host=os.getenv("DB_HOST"),
-    port=int(os.getenv("DB_PORT")),
+    port=int(os.getenv("DB_PORT", 3306)),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME"),
